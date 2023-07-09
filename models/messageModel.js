fa05f641dda83f496dd6d4829454324e59d697e1
Sagar2401 <prajaptisagar81410@gmail.com>
2023-07-09 13:30:42 +0530

    chat

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,15 +1,35 @@
+// const mongoose = require("mongoose");
+// const messageSchema = mongoose.Schema(
+//   {
+//     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//     content: { type: String, trim: true },
+//     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+//     readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Message = new mongoose.model("Message", messageSchema);
+// module.exports = Message;
 const mongoose = require("mongoose");
-const messageSchema = mongoose.Schema(
+
+const MessageSchema = mongoose.Schema(
   {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    content: { type: String, trim: true },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    message: {
+      text: { type: String, required: true },
+    },
+    users: Array,
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Message = new mongoose.model("Message", messageSchema);
-module.exports = Message;
+module.exports = mongoose.model("Messages", MessageSchema);
--- routes/message.js
+++ routes/message.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const route = express.Router();
+// const express = require("express");
+// const route = express.Router();
 
-// Import the controller
-// const messageController = require("../controllers/message");
+// // Import the controller
+// // const messageController = require("../controllers/message");
 
-route.get("/", (req, res) => {
-  res.send("Hello World from message.js");
-});
+// route.get("/", (req, res) => {
+//   res.send("Hello World from message.js");
+// });
 
-module.exports = route;
+// module.exports = route;
+const { addMessage, getMessages } = require("../controllers/chat");
+const router = require("express").Router();
+
+router.post("/addmsg", addMessage);
+router.post("/getmsg", getMessages);
+
+module.exports = router;
